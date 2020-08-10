2e1945f644cad5b85c6b124b2d6b4bdaf5a96f14
Ralf Bitter <rabit@revigniter.com>
2020-08-10 13:30:05 +0200

    Extend table definitions to meet JSON Feed 1.1 requirements

--- dbTableStructures/jsonfeedMySQLtable.sql
+++ dbTableStructures/jsonfeedMySQLtable.sql
@@ -1,8 +1,8 @@
--- ----------------------------
--- MySQL table structure for news
--- ----------------------------
+#-- ----------------------------
+#-- MySQL table definitions for news
+#-- ----------------------------
 CREATE TABLE `news` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `body` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   `postdate` datetime DEFAULT NULL,
@@ -10,4 +10,29 @@ CREATE TABLE `news` (
   `authorname` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `postvisible` enum('0','1') CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=89 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+
+
+#-- ----------------------------
+#-- MySQL table definitions for authors
+#-- ----------------------------
+CREATE TABLE `authors` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `url` varchar(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `avatar` varchar(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+
+
+#-- ----------------------------
+#-- MySQL table definitions for news_authors
+#-- ----------------------------
+CREATE TABLE `news_authors` (
+  `news_id` int(11) unsigned NOT NULL,
+  `authors_id` int(11) unsigned NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
